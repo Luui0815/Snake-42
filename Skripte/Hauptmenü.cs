43cc64f438a58a2607e2a478de418013ce88cabd
Luui0815 <147992866+Luui0815@users.noreply.github.com>
2024-04-24 19:34:43 +0200

    Debuggen sollte gehen

--- Skripte/Hauptmenü.cs
+++ Skripte/Hauptmenü.cs
@@ -3,19 +3,15 @@ using System;
 
 public class Hauptmenü : Control
 {
-	public override void _Ready()
-	{
 
-	}
+    public override void _Ready()
+    {
+        GD.Print("Hallo Welt!");
+    }
 
-	public override void _Process(float delta)
-	{
-	  
-	}
+    public void _on_Verbindung_erstellen_pressed()
+    {
+        GetTree().ChangeScene("res://Szenen/Verbindungseinstellungen.tscn");
+    }
 
-	public void _on_VerbindungAufbauen_pressed()
-	{
-		//GetTree().ChangeSceneToFile("res://scene/ui/LevelSelectionMenu.tscn"); -> so war es in Godot4
-		GetTree().ChangeScene("res://Szenen/Verbindungseinstellungen.tscn");
-	}
 }
