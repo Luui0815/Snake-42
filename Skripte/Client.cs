31f2a08ba30812ed40c7f7ee7771942876112b22
Luui0815 <147992866+Luui0815@users.noreply.github.com>
2024-04-25 21:29:41 +0200

    bissel weiter

--- Skripte/Client.cs
+++ Skripte/Client.cs
@@ -1,5 +1,9 @@
 using Godot;
 using System;
+using Snake42;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
 namespace Snake42
 {
     enum Nachricht
@@ -17,21 +21,37 @@ namespace Snake42
 }
 public class Client : Control
 {
-    private WebSocketClient WSPeer = new WebSocketClient();
+    private WebSocketClient _WSPeer = new WebSocketClient();
     public override void _Ready()
     {
-        ConnectToServer("127.0.0.1:8915");
+        
     }
 
     public void ConnectToServer(String ip)
     {
-       WSPeer.ConnectToUrl(ip);
+       _WSPeer.ConnectToUrl(ip);
        GD.Print("Client gestartet");
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public void _on_Client_starten_pressed()
+    {
+        ConnectToServer("ws://127.0.0.1:8915");
+    }
+
+    private void SendData(string Data)
+    {
+        JSONParseResult JsonNachricht = JSON.Parse(Data);
+        _WSPeer.GetPeer(1).PutPacket(JsonNachricht.ToString().ToUTF8());
+    }
+
+    public void _on_Testdaten_senden_pressed()
+    {
+        SendData("'Nachricht':" + Nachricht.join + "'data':" + "Hallo Welt");
+    }
+
+    public override void _Process(float delta)
+    {
+        // Port/Peer offen halten
+        _WSPeer.Poll();
+    }
 }
--- Skripte/Server.cs
+++ Skripte/Server.cs
@@ -1,26 +1,51 @@
 using Godot;
 using System;
 using Snake42;
+using System.Text;
+
 
 public class Server : Control
 {
-    private WebSocketServer WSPeer = new WebSocketServer();
+    private WebSocketServer _WSPeer = new WebSocketServer();
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        StartServer();
+        //Signale verbinden
+        GD.Print(_WSPeer.Connect("data_received",this,"PrintReceivedData"));
+        //_WSPeer.EmitSignal("data_received");
     }
 
     public void StartServer()
     {
-        WSPeer.Listen(8915);
+        _WSPeer.Listen(8915);
         GD.Print("Server lauscht");
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public void _on_Server_starten_pressed()
+    {
+        StartServer();
+    }
+
+    private String ConvertDataToString(byte[] packet)
+    {
+        string Nachricht = Encoding.UTF8.GetString(packet);
+        return JSON.Parse(Nachricht).ToString();
+    }
+
+    public void PrintReceivedData()
+    {
+        GD.Print(ConvertDataToString(_WSPeer.GetPeer(1).GetPacket()));
+    }
+
+    public override void _Process(float delta)
+    {
+        // gesendte Nachrichten empfangen
+        _WSPeer.Poll();
+
+        if(_WSPeer.HasSignal("data_received"))
+        {
+            //GD.Print("Ja");
+        }
+    }
 }
