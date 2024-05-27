8882ec5d0cdbd57c355a399c2d144098273ef99c
Luui0815 <147992866+Luui0815@users.noreply.github.com>
2024-05-27 21:20:09 +0200

    update gitignore

--- .mono/temp/obj/Debug/Snake 42.AssemblyInfo.cs
+++ .mono/temp/obj/Debug/Snake 42.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake 42")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e07be4296b5aba41405131b1e4c58d44fb24343")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd6fcb501bad5d91c7ec419d5431cb226c38d89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake 42")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake 42")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
