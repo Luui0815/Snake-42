b0073aa57879de646bef62cc2d70d99acbbe12f2
Luui0815 <147992866+Luui0815@users.noreply.github.com>
2024-05-27 21:20:48 +0200

    komm schon

--- .mono/temp/obj/Debug/Snake 42.AssemblyInfo.cs
+++ .mono/temp/obj/Debug/Snake 42.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake 42")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd6fcb501bad5d91c7ec419d5431cb226c38d89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8882ec5d0cdbd57c355a399c2d144098273ef99c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake 42")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake 42")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
